1588a804f0431b6a9de3ae72cdd7408f3cedae70
Mak-Dima <maksiutenko.dmytro88@gmail.com>
2024-12-07 17:50:24 -0500

    Register handler added.

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,6 +1,43 @@
-const config = {
-  backendUrl: process.env.REACT_APP_BACKEND_URL,
-};
+import urlConfig from `giftlink-frontend/src/config.js`
+import useAppContext from `giftlink-frontend/context/AuthContext.js`
+import useNavigate from `react-router-dom`
 
-console.log(`backendUrl in config.js: ${config.backendUrl}`)
-export {config as urlConfig}
+
+//Do these tasks inside the RegisterPage function, after the useStates definition
+//{{Insert code here}} //Task 4: Include a state for error message.
+const [showerr, setShowerr] = useState('');
+//{{Insert code here}} //Task 5: Create a local variable for `navigate`   and `setIsLoggedIn`.
+const navigate = useNavigate();
+const { setIsLoggedIn } = useAppContext();
+
+
+const handleRegister = async () => {
+    try{
+      const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+           metyhod: "POST",
+            headers: {'content-type': 'application/json',},
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        })
+
+        const json = await response.json();
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+            setIsLoggedIn(true);
+            navigate('/app')
+        }
+        if (json.error) {
+            setShowerr(json.error);
+            setShowerr(<div className="text-danger">{showerr}</div>)
+        }
+        
+    }catch (e) {
+        console.log("Error fetching details: " + e.message);
+    }
+}
